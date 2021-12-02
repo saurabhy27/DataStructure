3bf29f7348fbf1981cdff400e0a2bf0971b419e4
saurabhy27 <isaurabh2709@gmail.com>
2021-12-03 01:25:42 +0530

    binary tree

--- BinaryTree/BTree.py
+++ BinaryTree/BTree.py
@@ -88,4 +88,98 @@ class BinaryTree:
         node.left = self.constructPostOrderTree(InOrder, postOrder, start, inOrderIndex - 1)
         return node
 
+    def insertFirstInLNRPos(self, root, node):
+        if root is None:
+            return
+        if root.left is None and root.right is not None:
+            root.left = node
+            print("Left Node Not available {}")
+        if root.right is None and root.left is not None:
+            root.right = node
+            print("Left Node Not available {}")
+        self.insertFirstInLNRPos(root.left, node)
+        print("Node Data {} ".format(root.data))
+        self.insertFirstInLNRPos(root.right, node)
 
+    def insertFirstEmptyPos(self, root, node8):
+        if root is None:
+            return
+        queue = [root]
+        while len(queue) > 0:
+            removedNode = queue.pop(0)
+            if removedNode.left is not None:
+                queue.append(removedNode.left)
+            else:
+                removedNode.left = node8
+                break
+            if removedNode.right is not None:
+                queue.append(removedNode.right)
+            else:
+                removedNode.right = node8
+                break
+        return root
+
+    def searchNodeValue(self, root, value):
+        if root is None:
+            return False
+        if root.data == value:
+            return True
+        res = self.searchNodeValue(root.left, value)
+        if res:
+            return True
+        res = self.searchNodeValue(root.right, value)
+        return res
+
+    def maxKeyValue(self, root, maxValue):
+        if root is None:
+            return
+        if maxValue[0] <= root.data:
+            maxValue[0] = root.data
+        self.maxKeyValue(root.left, maxValue)
+        self.maxKeyValue(root.right, maxValue)
+
+    def deleteNode(self, root, node):
+        if root is None:
+            return None
+        queue = [root]
+        while len(queue) > 0:
+            removedNode = queue.pop(0)
+            if removedNode is node:
+                removedNode = None
+                return
+            if removedNode.left is not None:
+                if removedNode.left == node:
+                    removedNode.left = None
+                    return
+                queue.append(removedNode.left)
+            if removedNode.right is not None:
+                if removedNode.right == node:
+                    removedNode.right = None
+                    return
+                queue.append(removedNode.right)
+        return
+
+    def findDeepestNodeAndKeyNodeAndDelete(self, root, key):
+        if root is None:
+            return None
+        if root.left is None and root.right is None:
+            if root.data == key:
+                return None
+            else:
+                return root
+        queue = [root]
+        keyNode = None
+        removedNode = None
+        while len(queue) > 0:
+            removedNode = queue.pop(0)
+            if removedNode.data == key:
+                keyNode = removedNode
+            if removedNode.left is not None:
+                queue.append(removedNode.left)
+            if removedNode.right is not None:
+                queue.append(removedNode.right)
+        if keyNode:
+            x = removedNode.data
+            self.deleteNode(root, removedNode)
+            keyNode.data = x
+        return root
--- BinaryTree/main.py
+++ BinaryTree/main.py
@@ -1,5 +1,3 @@
-from builtins import int
-
 from BinaryTree.BTree import BinaryTree
 from BinaryTree.BNode import BNode
 
@@ -15,14 +13,14 @@ if __name__ == '__main__':
     binaryTree.head = root
     root.left = node2
     node2.left = node4
-    node2.right = node5
+    # node2.right = node5
     root.right = node3
     node3.left = node7
     node3.right = node6
     print("Preorder ")
     # binaryTree.preorder(root)
     print("Inorder ")
-    # binaryTree.inorder(root)
+    binaryTree.inorder(root)
     print("postorder ")
     # binaryTree.postorder(root)
     print("Label ")
@@ -30,13 +28,33 @@ if __name__ == '__main__':
     # print("Height of a binary tree {}".format(binaryTree.height(root)))
 
     print("Construct PreOrder Binary Tree ")
-    inOrder = ['D', 'B', 'E', 'A', 'F', 'C']
-    preOrder = ['A', 'B', 'D', 'E', 'C' , 'F']
+    # inOrder = ['D', 'B', 'E', 'A', 'F', 'C']
+    # preOrder = ['A', 'B', 'D', 'E', 'C' , 'F']
     # rootNode = binaryTree.constructPreOrderTree(inOrder, preOrder, 0, len(inOrder) - 1)
     # binaryTree.preorder(rootNode)
 
     print("Construct PostOrder Binary Tree ")
-    In = [4, 8, 2, 5, 1, 6, 3, 7]
-    post = [8, 4, 5, 2, 6, 7, 3, 1]
-    rootNode = binaryTree.constructPostOrderTree(In, post, 0, len(post) - 1)
-    binaryTree.preorder(rootNode)
+    # In = [4, 8, 2, 5, 1, 6, 3, 7]
+    # post = [8, 4, 5, 2, 6, 7, 3, 1]
+    # rootNode = binaryTree.constructPostOrderTree(In, post, 0, len(post) - 1)
+    # binaryTree.preorder(rootNode)
+
+    print("Insert node at first possible of LNR ")
+    node8 = BNode(8)
+    # binaryTree.insertFirstInLNRPos(root, node8)
+
+    print("Insert node at first empty position ")
+    # root = binaryTree.insertFirstEmptyPos(root, node8)
+    # binaryTree.labelWiseTransversal(root)
+
+    print("search node value exists in tree ")
+    # print(binaryTree.searchNodeValue(root, 8))
+
+    print("Maximum value key ")
+    # maxValue = [0]
+    # binaryTree.maxKeyValue(root, maxValue)
+    # print(maxValue)
+
+    print("Delete node from binary tree ")
+    root = binaryTree.findDeepestNodeAndKeyNodeAndDelete(root, 2)
+    binaryTree.labelWiseTransversal(root)
